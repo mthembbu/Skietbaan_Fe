f95b38ab6491e33f36ce94f25e998973af6dcd0f
mthembbu <bmthembu@retrorabbit.co.za>
2019-01-30 17:49:12 +0200

    SASS setup
    Jest Setup

---
+++ skietbaan_fe/src/api/__tests__/github.test.js
@@ -0,0 +1,15 @@
+
+/* eslint-env jest */
+jest.mock('../request')
+const github = require('../github')
+
+// A simple example test
+describe('#getUser() using Promises', () => {
+  it('should load user data', () => {
+    return github.getUser('vnglst')
+    .then(data => {
+      expect(data).toBeDefined()
+      expect(data.entity.name).toEqual('Koen van Gilst')
+    })
+  })
+})
