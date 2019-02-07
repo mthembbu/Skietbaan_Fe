558f8612e31fa6caff44b063d1334e224501e193
mthembbu <bmthembu@retrorabbit.co.za>
2019-02-07 13:40:13 +0200

    Changed landing.js

--- skietbaan_fe/src/App.js
+++ skietbaan_fe/src/App.js
@@ -4,10 +4,9 @@ import { Provider } from 'react-redux';
 import store from './store';
 import { getUser } from './api/github';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
-import About from './components/About';
-import Welcome from './components/Welcome';
 import Login from './components/Login';
-import Register from './components/Register';
+import RegisterMember from './components/RegisterMember';
+import landing from './components/landing';
 class App extends Component {
 	componentDidMount() {
 		getUser('vnglst').then((data) => {
@@ -23,9 +22,7 @@ class App extends Component {
 							<Route path="/home" component={landing} exact />
 							<Route path="/login" component={Login} exact />
 							<Route path="/" component={Login} exact />
-							<Route path="/register" component={Register} exact />
-							<Route path="/about" component={About} exact />
-							<Route path="/welcome" component={Welcome} exact />
+							<Route path="/register" component={RegisterMember} exact />						
 							<Redirect from="/" to="/" />
 						</Switch>
 					</BrowserRouter>
