0b63ab4474859063014b8096e5328e2f5a6f7eaa
mthembbu <bmthembu@retrorabbit.co.za>
2019-02-07 10:42:51 +0200

    Change made

--- skietbaan_fe/src/App.js
+++ skietbaan_fe/src/App.js
@@ -1,39 +1,39 @@
-import React, { Component } from "react";
-import "./App.scss";
-import { Provider } from "react-redux";
-import store from "./store";
-import { getUser } from "./api/github";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
-import About from "./components/About";
-import Welcome from "./components/Welcome";
-import Login from "./components/Login";
-import Register from "./components/Register";
+import React, { Component } from 'react';
+import './App.scss';
+import { Provider } from 'react-redux';
+import store from './store';
+import { getUser } from './api/github';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+import About from './components/About';
+import Welcome from './components/Welcome';
+import Login from './components/Login';
+import Register from './components/Register';
 
 class App extends Component {
-  componentDidMount() {
-    getUser("vnglst").then(data => {
-      this.setState({ user: data.entity });
-    });
-  }
+	componentDidMount() {
+		getUser('vnglst').then((data) => {
+			this.setState({ user: data.entity });
+		});
+	}
 
-  render() {
-    return (
-      <Provider store={store}>
-        <div className="App">
-          <BrowserRouter>
-            <Switch>
-              <Route path="/home" component={landing} exact />
-              <Route path="/login" component={Login} exact />
-              <Route path="/" component={Login} exact />
-              <Route path="/register" component={Register} exact />
-              <Route path="/about" component={About} exact />
-              <Route path="/welcome" component={Welcome} exact />
-              <Redirect from="/" to="/" />
-            </Switch>
-          </BrowserRouter>
-        </div>
-      </Provider>
-    );
-  }
+	render() {
+		return (
+			<Provider store={store}>
+				<div className="App">
+					<BrowserRouter>
+						<Switch>
+							<Route path="/home" component={landing} exact />
+							<Route path="/login" component={Login} exact />
+							<Route path="/" component={Login} exact />
+							<Route path="/register" component={Register} exact />
+							<Route path="/about" component={About} exact />
+							<Route path="/welcome" component={Welcome} exact />
+							<Redirect from="/" to="/" />
+						</Switch>
+					</BrowserRouter>
+				</div>
+			</Provider>
+		);
+	}
 }
 export default App;
