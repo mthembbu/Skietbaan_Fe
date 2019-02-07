58e671e9e837e81f4df4bb11b4098580af266e0d
ilzeLourens <ilourens@retrorabbit.co.za>
2019-02-07 10:06:42 +0200

    Did requested changes

--- skietbaan_fe/src/components/Login.js
+++ skietbaan_fe/src/components/Login.js
@@ -1,101 +1,78 @@
 import React, { Component } from 'react';
-import {
-  Container, Col,
-  FormGroup, Label, Input,Button,Form
-} from 'reactstrap';
+import { Container, Col,FormGroup, Label, Input, Button, Form} from 'reactstrap';
 import '../components/LoginStyles.css';
+import { validatePassword, validateEmail, validateUsername } from './Validators.js';
 
-  function validatePassword(str) {
-      var re = /(?=.*\d)(?=.*[a-z]).{6,}/;
-      return re.test(str);
-    }
-
-    function validateEmail(email) {
-      var re = /.+@.+\..+/;
-      return re.test(String(email).toLowerCase());
-    }
-
-    function validateUsername(username) {
-      var re = /[a-zA-Z]/;
-        return !re.test(String(username));
-    }
-    
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      usernameValue : "",
-      emailValue : "",
-      passwordValue:"",
-      invalidPassword:false,
-      invalidEmail:false,
+      usernameValue: "",
+      emailValue: "",
+      passwordValue: "",
+      invalidPassword: false,
+      invalidEmail: false,
       invalidUsername: false,
-      validForm:false
+      validForm: false
     }
     this.Login = this.Login.bind(this);
     this.handleChange = this.handleChange.bind(this);
+
   }
   handleChange({ target }) {
     this.setState({
       [target.name]: target.value,
-      invalidPassword:false,
+      invalidPassword: false,
       invalidEmail: false,
       invalidUsername: false
     });
-
-    var Valid = true;
-    if(!validatePassword(this.state.passwordValue))
-    {
-        this.setState({
-          invalidPassword : true
-        });
-        Valid = false;
+  let stateUpdate ={};
+    var isValid = true;
+    if (!validatePassword(this.state.passwordValue)) {
+      this.setState({
+        invalidPassword: true
+      });
+      isValid = false;
     }
-    if(!validateEmail(this.state.emailValue))
-    {
-        this.setState({
-          invalidEmail : true
-        });
-        Valid = false;
+    if (!validateEmail(this.state.emailValue)) {
+      this.setState({
+        invalidEmail: true
+      });
+      isValid = false;
     }
-    if(validateUsername(this.state.usernameValue))
-    {
-        this.setState({
-          invalidUsername : true
-        });
-        Valid = false;
+    if (validateUsername(this.state.usernameValue)) {
+      this.setState({
+        invalidUsername: true
+      });
+      isValid = false;
     }
     this.setState({
-      validForm:Valid
+      validForm: isValid
     });
   }
-  
-  Login() {
-    if(this.state.validForm)
-    {
-    var RequestObject = {
-      "Username":this.state.usernameValue,
-      "Password":this.state.emailValue,
-      "Surname":this.state.passwordValue
-    }
-    console.log(RequestObject);
 
-    fetch('http://skietbaan.retrotest.co.za/api/values', {
-      method: 'post',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-       },
-       body: JSON.stringify(RequestObject)
+  Login() {
+    if (this.state.validForm) {
+      var RequestObject = {
+        "Username": this.state.usernameValue,
+        "Password": this.state.emailValue,
+        "Surname": this.state.passwordValue
+      }
+      const BASE_URL = 'http://skietbaan.retrotest.co.za/';
+      fetch(`${BASE_URL}`,"/api/values", {
+        method: 'post',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(RequestObject)
       }).then(function(response) {
         return response.json();
-      }).then(function(data) {
-        console.log('Backend output', data);
+      }).then( function(data) {
       }).catch(function() {
-        console.log("error");
       });
-      }
     }
+  }
 
   render() {
     let invalidPasswordMessage;
@@ -107,66 +84,66 @@ class App extends Component {
     }
     if (this.state.invalidEmail) {
       invalidEmailMessage = <div className="invalid-message">Invalid email</div>;
-    } 
+    }
     if (this.state.invalidUsername) {
       invalidUsernameMessage = <div className="invalid-message">Please enter your username</div>;
-    } 
+    }
 
     return (
       <Container className="App">
         <div className="centre-login">
-        <Form className="form">
-        <h2>Login</h2>
-          <Col className="no-padding">
-            <FormGroup>
-              <Label>Username</Label>
-              <Input
-                type="text"
-                name="usernameValue"
-                id="us"
-                placeholder="username"
-                value={ this.state.usernameValue }
-                onChange={ this.handleChange }
-                className={this.state.invalidUsername ? "invalid":""}
-              />
-              {invalidUsernameMessage}
-            </FormGroup>
-          </Col>
-          <Col className="no-padding">
-            <FormGroup>
-              <Label for="examplePassword">Email</Label>
-              <Input
-                type="email"
-                name="emailValue"
-                id="LoginEmail"
-                placeholder="Email"
-                value={ this.state.emailValue }
-                onChange={ this.handleChange }
-                className={this.state.invalidEmail ? "invalid":""}
-              />
-              {invalidEmailMessage}
-            </FormGroup>
-          </Col>
-          <Col className="no-padding">
-            <FormGroup>
-              <Label for="examplePassword">Password</Label>
-              <Input
-                type="password"
-                name="passwordValue"
-                id="examplePassword"
-                placeholder="********"
-                value={ this.state.passwordValue }
-                onChange={ this.handleChange }
-                className={this.state.invalidPassword ? "invalid":""}
-              />
-              {invalidPasswordMessage}
-            </FormGroup>
-          </Col>
-          <Button onClick={this.Login} className={this.state.validForm ? "button-valid":"button-invalid"}>Submit</Button>
+          <Form className="form">
+            <h2>Login</h2>
+            <Col className="no-padding">
+              <FormGroup>
+                <Label>Username</Label>
+                <Input
+                  type="text"
+                  name="usernameValue"
+                  id="us"
+                  placeholder="username"
+                  value={this.state.usernameValue}
+                  onChange={this.handleChange}
+                  className={this.state.invalidUsername ? "invalid" : ""}
+                />
+                {invalidUsernameMessage}
+              </FormGroup>
+            </Col>
+            <Col className="no-padding">
+              <FormGroup>
+                <Label for="examplePassword">Email</Label>
+                <Input
+                  type="email"
+                  name="emailValue"
+                  id="LoginEmail"
+                  placeholder="Email"
+                  value={this.state.emailValue}
+                  onChange={this.handleChange}
+                  className={this.state.invalidEmail ? "invalid" : ""}
+                />
+                {invalidEmailMessage}
+              </FormGroup>
+            </Col>
+            <Col className="no-padding">
+              <FormGroup>
+                <Label for="examplePassword">Password</Label>
+                <Input
+                  type="password"
+                  name="passwordValue"
+                  id="examplePassword"
+                  placeholder="********"
+                  value={this.state.passwordValue}
+                  onChange={this.handleChange}
+                  className={this.state.invalidPassword ? "invalid" : ""}
+                />
+                {invalidPasswordMessage}
+              </FormGroup>
+            </Col>
+            <Button onClick={this.Login} className={this.state.validForm ? "button-valid" : "button-invalid"}>Submit</Button>
           </Form>
-          </div >
+        </div >
       </Container>
-      
+
     );
   }
 }
