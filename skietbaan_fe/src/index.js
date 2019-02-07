2b094dc1a353510146c98464ea86dc9e1e02842a
mthembbu <bmthembu@retrorabbit.co.za>
2019-02-07 19:26:40 +0200

    Got read of the GC

--- skietbaan_fe/src/index.js
+++ skietbaan_fe/src/index.js
@@ -5,17 +5,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { Provider } from 'react-redux'
-import configureStore from './store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-{/** TODO GC. Reason: Still need to confirm if the App.js browserouter
- can work without the <Provider>*/}
-/** ReactDOM.render(<Provider store={configureStore()}>
-<App />
-</Provider>,
-document.getElementById('root')
-);  */
-//registerServiceWorker();
 serviceWorker.register();
