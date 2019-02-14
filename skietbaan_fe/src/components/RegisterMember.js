58ceefe75c635570a8a306791a96613687547af7
DinolanNaidoo <dnaidoo@retrorabbit.co.za>
2019-02-14 16:18:55 +0200

    Removed positions

--- skietbaan_fe/src/components/RegisterMember.js
+++ skietbaan_fe/src/components/RegisterMember.js
@@ -69,7 +69,7 @@ class App extends Component {
       "entryDate": document.getElementById("entrydate").value + "T00:00:00",
       "memberExpiry": document.getElementById("expdate").value + "T00:00:00"
     }
-    fetch("https://api.skietbaan.retrotest.co.za/api/Features/Update", {
+    fetch("https://api.skietbaan.retrotest.co.za/api/User/Update", {
       method: 'Put',
       headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
       body: JSON.stringify(RequestObject)
@@ -143,7 +143,7 @@ class App extends Component {
               <Button onClick={this.UpdateMember} >Submit</Button>
             </Col>
           </Form>
-          <ToastContainer store={ToastStore} position={ToastContainer.POSITION.TOP_CENTER} lightBackground />
+          <ToastContainer store={ToastStore} position={ToastContainer.TOP_CENTER} lightBackground />
         </div >
       </Container>
     );
