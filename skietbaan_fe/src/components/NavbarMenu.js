14573760d7a915c99bff1c880284a59e3ed2e866
mthembbu <bmthembu@retrorabbit.co.za>
2019-02-20 11:28:09 +0200

    changed the bootstrap code

--- skietbaan_fe/src/components/NavbarMenu.js
+++ skietbaan_fe/src/components/NavbarMenu.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Navbar, NavDropdown, Form, Nav, Container } from 'react-bootstrap';
 import '../scss/navbar.css';
+import LB from '../resources/trophy.png';
+
 class NavbarMenu extends Component {
 	render() {
 		// rendering the basic navbar within the render class
@@ -30,7 +32,30 @@ class NavbarMenu extends Component {
 						</Form>
 					</Navbar.Collapse>
 				</Navbar>
-			</div>	
+										 {/*<img src="../resources/trophy.png" width="30" height="30" alt="Sbuda"/>*/}
+									 <i class="fa fa-trophy" width="30"><span class="badge badge-danger">2</span></i>
+									<a class="nav-link" href="/">
+									<a class="nav-link" href="/">
+										{/** <img src="../../public/img/trophy.png" width="30" height="30" alt=""> </img>*/}
+										<i class="fa fa-file-o">
+											<span class="badge badge-danger">11</span>
+										</i>
+										DOCUMENTS
+									</a>
+								</li>
+							</ul>
+							{/**<div>
+						<form class="form-inline my-2 my-md-0">
+							 <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
+							<button class="btn btn-outline-success my-4 my-md-0" type="submit">
+								Search
+							</button>
+						</form>
+						</div> */}
+						</div>
+					</div>
+				</nav>
+			</div>
 		);
 	}
 }
