6ce33d72b9c364ab8b51b5f046e9e932fa9b9f6b
mthembbu <bmthembu@retrorabbit.co.za>
2019-02-20 14:22:32 +0200

    Cleared GC code from the NavbarMenu.js, GC for img has a TODO

--- skietbaan_fe/src/components/NavbarMenu.js
+++ skietbaan_fe/src/components/NavbarMenu.js
@@ -1,53 +1,16 @@
 import React, { Component } from 'react';
 import { Navbar, NavDropdown, Form, Nav, Container } from 'react-bootstrap';
 import '../scss/navbar.css';
-import LB from '../resources/trophy.png';
 
 class NavbarMenu extends Component {
 	render() {
 		// rendering the basic navbar within the render class
 		return (
 			<div>
-				{/** *<Navbar bg="light" expand="lg">
-					<Navbar.Brand href="/home">SKIETBAAN APP</Navbar.Brand>
-					<Navbar.Toggle aria-controls="basic-navbar-nav" />
-					<Navbar.Collapse id="basic-navbar-nav">
-						<Nav className="mr-auto">
-							<Nav.Link href="/home">Leader Board</Nav.Link>
-							<Nav.Link href="/scorecapture">Score Capture</Nav.Link>
-							<NavDropdown title="Create" id="basic-nav-dropdown" variant="outline-dark">
-								<NavDropdown.Item href="/new-competition">New Competition</NavDropdown.Item>
-								<NavDropdown.Item href="/groupsname">New Groups</NavDropdown.Item>
-								<NavDropdown.Item href="/register-member">New Members</NavDropdown.Item>
-								<NavDropdown.Divider />
-							</NavDropdown>
-							<Nav.Link href="/create">
-								<Figure>
-									<Figure.Image
-										width={25}
-										height={80}
-										alt="25x120"
-										src="img/add_img.png"
-										align="centre"
-									/>
-									<Figure.Caption>Create</Figure.Caption>
-								</Figure>
-							</Nav.Link>
-						</Nav>
-						<Form inline>
-							<NavDropdown title="Username" id="basic-nav-dropdown" variant="outline-dark">
-								<NavDropdown.Item href="/home">Profile</NavDropdown.Item>
-								<NavDropdown.Item href="/notifications">Notifications</NavDropdown.Item>
-								<NavDropdown.Item href="/home">Documents</NavDropdown.Item>
-							</NavDropdown>
-						</Form>
-					</Navbar.Collapse>
-				</Navbar> */}<nav class="navbar navbar-icon-top navbar-expand-lg navbar-dark bg-dark fixed-top">
+				<nav class="navbar navbar-icon-top navbar-expand-lg navbar-dark bg-dark fixed-top">
 					<a class="navbar-brand" href="/home">
 						{/** *SKIETBAAN*/}
 					</a>
-			{/**The part of the Navigation-Bar that remains in a mobile device */}
-					
 					<button
 						id="h-btn"
 						class="navbar-toggler"
@@ -65,14 +28,16 @@ class NavbarMenu extends Component {
 							<ul class="navbar-nav mr-auto nav-fill">
 								<li class="nav-item ">
 									<a class="nav-link" href="/home">
-										 {/*<img src="../resources/trophy.png" width="30" height="30" alt="Sbuda"/>*/}
-									 <i class="fa fa-trophy" width="30"><span class="badge badge-danger">2</span></i>
+										{/*GC TODO: Refactor for Image displays: <img src="../resources/trophy.png" width="30" height="30" alt="Sbuda"/>*/}
+										<i class="fa fa-trophy" width="30">
+											<span class="badge badge-danger">2</span>
+										</i>
 										LEADERBOARD
 									</a>
 								</li>
 								<li class="nav-item">
 									<a class="nav-link" href="/create">
-										{/** <img src="../../public/img/trophy.png" width="30" height="30" alt=""> </img>*/}
+										{/***GC TODO: Refactor for Image displays: <img src="../../public/img/trophy.png" width="30" height="30" alt=""> </img>*/}
 										<i class="fa fa-plus-circle">
 											<span class="badge badge-danger" />
 										</i>
@@ -81,7 +46,7 @@ class NavbarMenu extends Component {
 								</li>
 								<li class="nav-item">
 									<a class="nav-link " href="/scorecapture">
-										{/** <img src="../../public/img/trophy.png" width="30" height="30" alt=""> </img>*/}
+										{/***GC TODO: Refactor for Image displays: <img src="../../public/img/trophy.png" width="30" height="30" alt=""> </img>*/}
 										<i class="fa fa-dot-circle-o">
 											<span class="badge badge-warning" />
 										</i>
@@ -90,7 +55,7 @@ class NavbarMenu extends Component {
 								</li>
 								<li class="nav-item">
 									<a class="nav-link" href="/notifications">
-										{/** <img src="../../public/img/trophy.png" width="30" height="30" alt=""> </img>*/}
+										{/***GC TODO: Refactor for Image displays: <img src="../../public/img/trophy.png" width="30" height="30" alt=""> </img>*/}
 										<i class="fa fa-bell">
 											<span class="badge badge-danger">11</span>
 										</i>
@@ -99,7 +64,7 @@ class NavbarMenu extends Component {
 								</li>
 								<li class="nav-item">
 									<a class="nav-link" href="/">
-										{/** <img src="../../public/img/trophy.png" width="30" height="30" alt=""> </img>*/}
+										{/***GC TODO: Refactor for Image displays: <img src="../../public/img/trophy.png" width="30" height="30" alt=""> </img>*/}
 										<i class="fa fa-user">
 											<span class="badge badge-success" />
 										</i>
@@ -109,7 +74,7 @@ class NavbarMenu extends Component {
 
 								<li class="nav-item">
 									<a class="nav-link" href="/">
-										{/** <img src="../../public/img/trophy.png" width="30" height="30" alt=""> </img>*/}
+										{/***GC TODO: Refactor for Image displays: <img src="../../public/img/trophy.png" width="30" height="30" alt=""> </img>*/}
 										<i class="fa fa-file-o">
 											<span class="badge badge-danger">11</span>
 										</i>
@@ -117,14 +82,6 @@ class NavbarMenu extends Component {
 									</a>
 								</li>
 							</ul>
-							{/**<div>
-						<form class="form-inline my-2 my-md-0">
-							 <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search" />
-							<button class="btn btn-outline-success my-4 my-md-0" type="submit">
-								Search
-							</button>
-						</form>
-						</div> */}
 						</div>
 					</div>
 				</nav>
