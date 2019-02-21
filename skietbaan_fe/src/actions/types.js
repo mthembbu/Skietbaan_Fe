b052775eb6c7096d2e51a23455f5b6705866c01f
ilzeLourens <46757625+ilzeLourens@users.noreply.github.com>
2019-02-21 13:04:07 +0200

    Delete types.js

--- skietbaan_fe/src/actions/types.js
+++
@@ -1,7 +0,0 @@
-export const ADD_BOOKMARK = 'ADD_BOOKMARK';
-export const DELETE_BOOKMARK = 'DELETE_BOOKMARK';
-export const FETCH_POSTS = 'FETCH_POSTS';
-export const NEW_POST = 'NEW_POST';
-export const UPDATE_GROUPNAME = 'UPDATE_GROUPNAME';
-export const NEW_COMP = 'NEW_COMP';
-export const FETCH_COMP = 'NEW_COMP';
