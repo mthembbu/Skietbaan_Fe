aae78f4d62aa0063852baa4c0916cca503c7b02b
Zintle Skosana <zskosana@retrorabbit.co.za>
2019-02-28 11:48:33 +0200

    Updated Notification Page... Working on routing users

--- skietbaan_fe/src/components/Notifications.js
+++ skietbaan_fe/src/components/Notifications.js
@@ -1,9 +1,6 @@
 import React, { Component } from "react";
 import "../components/NotificationsStyle.css";
-import { Table, Button, Col } from "reactstrap";
 import { getCookie } from "./cookie";
-import  history  from "./history";
-
 class notification extends Component {
   constructor(props) {
     super(props);
@@ -17,30 +14,21 @@ class notification extends Component {
       deleteClicked: false,
       viewClicked: false
     };
-    this.handleView = this.handleView.bind(this);
-    this.handleRead = this.handleRead.bind(this);
     this.onDelete = this.onDelete.bind(this);
   }
 
-  handleRead() {
-    if (this.state.isRead) {
-      //
-    }
-  }
+  onDelete = key => {
+    let id = this.state.array[key].id;
+    let newArray = this.state.array;
+    newArray.splice(key, 1);
+    this.setState({ array: newArray });
 
-  onDelete=key =>{
-  console.log(key);
-    let id = this.state.array[key].id
-    let newArray  = this.state.array;
-    newArray.splice(key,1)
-   this.setState({array : newArray});
-   
-     fetch(`http://localhost:63474/api/Notification/${id}`, {
+    fetch(`http://localhost:63474/api/Notification/${id}`, {
       method: "Delete"
     }).catch(err => {
       console.error("err", err);
     });
-  }
+  };
 
   initialState() {
     return this.setState({
@@ -48,11 +36,27 @@ class notification extends Component {
     });
   }
 
-  handleView() {
-    if (this.state.viewClicked) {
-      //redirect to correct page
-    } else {
+  onClick_View = Notification => {
+    if (!this.state.viewClicked) {
+        if (Notification === "Award") {
+          window.location("./Leaderboard");
+        } else if (Notification === "Confirmation") {
+          window.location("");
+        } else if (Notification === "Renewal") {
+          window.location("");
+        } else if (Notification === "Competition") {
+          window.location("./Competition");
+        } else if (Notification === "Document") {
+          window.location("");
+        } else if (Notification === "Group") {
+          window.location("./Group");
+        }else{
+          return null;
+        }
     }
+    this.setState({
+      typeOfNotification:Notification
+    })
   }
 
   componentDidMount() {
@@ -60,88 +64,63 @@ class notification extends Component {
       var token = document.cookie;
       fetch("http://localhost:63474/api/Notification?" + token)
         .then(response => response.json())
-        .then(data =>{
+        .then(data => {
           this.setState({
             array: data
-          })
-          console.log(this.state.array)}
-        )
-        .catch(function(data) {
-          console.log(data);
-        });
+          });
+        })
+        .catch(function(data) {});
     }
   }
 
-  redirect(){
-    history.push("/notify");
-  }
+  // redirect() {
+  //   history.push("/notify");
+  // }
 
   render() {
     const headingItems = (
       <div className="PageHeading">
-        <img src={require("./Notification-Img/redirect2.png")} onClick={this.redirect.bind(this)} alt="redirect" />
-        <b>Notifications</b>
-      </div>
-    );
-
-    const viewButtons = (
-      <div>
-        <a href="">
-          <img
-            src={require("./Notification-Img/eyeIcon2.png")}
-            alt="redirect"
-          />
-        </a>
-      </div>
-    );
-
-    const threeDots = (
-      <div>
-          
-      </div>
-    );
-
-    const deleteButtons = (
-      <div>
-          <img
-            onClick={this.onDelete}
-            className="Buttons"
-            src={require("./Notification-Img/delete2.png")}
-            alt="redirect"
-          />
+        <b className="pageheading">Notifications</b>
       </div>
     );
 
     const postItems = (
-      <Table>
-        <tbody>
-          {this.state.array.map((post,i) => (
-            <tr key={i} >
-              <td>{post.notificationMessage}</td>
-              <td>{viewButtons}</td>
-              <td><img
-           onClick={()=>{this.onDelete(i)}}
-            className="Buttons"
-            src={require("./Notification-Img/3dots.png")}
-            alt="redirect"
-          /></td>
+      <table className="postItems">
+        <tbody className="">
+          {this.state.array.map((post, i) => (
+            <tr className="trClass" key={i}>
+              <td className="tdNotification text">
+                <a 
+                  className="text"
+                  href=""
+                  onClick={() => {
+                    this.onClick_View(post.typeOfNotification);
+                  }}
+                >
+                  {post.notificationMessage}
+                </a>
+              </td>
+              <td className="tdDelete">
+                <img
+                  onClick={() => {
+                    this.onDelete(i);
+                  }}
+                  className="images"
+                  src={require("./Notification-Img/delete.png")}
+                  alt="redirect"
+                />
+              </td>
+              <hr />
             </tr>
           ))}
         </tbody>
-      </Table>
+      </table>
     );
 
     return (
-      <div>
+      <div className="bodyClass">
         <div>{headingItems}</div>
         <div>{postItems}</div>
-        <div>
-          <Col>
-            <Button className="buttonCss" onClick={this.redirect}>
-              Load older notifications
-            </Button>
-          </Col>
-        </div>
       </div>
     );
   }
