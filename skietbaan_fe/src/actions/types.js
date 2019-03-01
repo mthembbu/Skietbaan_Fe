f6a09d8ca5a2e9f390edc891c5239de09d9cf64b
kyle-manganyi <tmanganyi@retrorabbit.co.za>
2019-03-01 11:10:08 +0200

    working FE

--- skietbaan_fe/src/actions/types.js
+++ skietbaan_fe/src/actions/types.js
@@ -4,24 +4,8 @@ export const FETCH_POSTS = 'FETCH_POSTS';
 export const NEW_POST = 'NEW_POST';
 export const NEW_COMP = 'NEW_COMP';
 export const FETCH_COMP = 'NEW_COMP';
-export const BASE_URL = "http://localhost:5000";
+export const BASE_URL = "http://localhost:50962";
 //leaderboard types
 export const FETCH_LEADERBOARDFILTER_DATA = 'FETCH_LEADERBOARDFILTER_DATA';
 export const FETCH_LEADERBOARDTABLE_DATA = 'FETCH_LEADERBOARDTABLE_DATA';
-export const URL = "http://localhost:5000"; 
-
-export const SendMail = (email)=>{
-    (async () => {
-        const rawResponse = await fetch(BASE_URL+`/api/Documents/Email/${email}`, {
-          method: 'PUT',
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            "email": email
-            })
-        });
-  })();
-}
-
+export const URL = "http://localhost:50962"; 
--- skietbaan_fe/src/components/Documents.js
+++ skietbaan_fe/src/components/Documents.js
@@ -1,121 +1,79 @@
 import React, { Component } from 'react';
 import { Label, Table, Input, Button } from 'reactstrap';
 import '../components/Documents.css';
+import { getCookie } from './cookie.js';
 import {BASE_URL} from '../actions/types.js';
 
 class Documents extends Component {
+
     constructor(props) {
         super(props);
         this.state = {
-            array: [],
-            email: ""
-        }
-        this.GetMembers = this.GetMembers.bind(this);
-        this.SendMail = this.SendMail.bind(this);
-    }
-    
-    GetMembers() {
-        fetch(BASE_URL+"/api/Features/SearchMember", {
-            method: 'Get',
-            headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
-        })
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (data) {
-                return data;
-            }).then(data => this.setState({
-                array: data.filter(function (datas) {
-                    return ((datas.username).startsWith(document.getElementById("usernameValue").value)
-                        || (datas.email).startsWith(document.getElementById("usernameValue").value));
-                })
-            }))
-            .catch(function () { });
+          value: "",
+          value2: "",
+        };
+
+        this.SendLOGS = this.SendLOGS.bind(this)
+        this.SendLOS = this.SendLOS.bind(this)
+      }
+
+    componentWillMount(){
+        let token =getCookie("token");
+        fetch("http://localhost:50962/api/Documents/UserLOGS/" + token)
+        .then(res=>res.json())
+        .then(data=>this.setState({value:data}))
+
+        fetch("http://localhost:50962/api/Documents/UserLOS/" + token)
+        .then(res=>res.json())
+        .then(data=>this.setState({value2:data}))
     }
 
-    SearchMember(index,user) {
-        this.setState({
-            email: user.email
-        });
-     
-        document.getElementById("usernameValue").value = user.email;
-        
-        fetch(BASE_URL,"/api/Features/Search?Username=" + user.name, {
-            method: 'Get',
-            headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
-        }).then(function (response) {
-            return response.json();
-        }).then(function (data) {
-                document.getElementById("usernameValue").value = data.email;
-            })
-            .catch(function () { });
+    SendLOGS(){
+        let token =getCookie("token");
+        fetch("http://localhost:50962/api/Documents/SendLOGS/" + token)
     }
 
-    SendMail(){
-        (async () => {
-            const rawResponse = await fetch(BASE_URL+`/api/Documents/Email/${this.state.email}`, {
-              method: 'PUT',
-              headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-              },
-              body: JSON.stringify({
-                "email": this.state.email
-                })
-            });
-      })();
+    SendLOS(){
+        let token =getCookie("token");
+        fetch("http://localhost:50962/api/Documents/SendLOS/" + token)
     }
 
-
+   
     render() {
-        const postItems = (
-            <Table striped hover condensed>
-                <tbody>
-                    {this.state.array.map((post, index) => (
-                        <tr key={post.id} onClick={() => this.SearchMember(index,post)}>
-                            <td>
-                                <b>{post.username}</b>
-                                <p>{post.email}</p>
-                            </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </Table>
-        );
+        console.log(this.state.value)
+        var urDivStyle ={
+            margin:'0',
+            padding: '2%'
+        }
+        var rowJustifyStyle={
+            backgroundColor: 'black',
+            height: '49px'
+        }
+        
         return (
             <div className="documents-container">
-                <div className="page-name">
-                    <h2>Documents</h2>
+                <div className="row justify-content-center" style={rowJustifyStyle} >
+                    <h1 className="page-name2"  style={urDivStyle}>Documents</h1>
                 </div>
-                <div className="documents-body">
-                    <div className="label-select-member">
-                        <Label>Select Member</Label>
-                    </div>
-                    <div className="search-name">
-                        <Input
-                            type="text"
-                            name="usernameValue"
-                            id="usernameValue"
-                            placeholder="Username or Email"
-                            value={this.state.email}
-                            onChange={this.GetMembers} />
-                    </div>
-                    <div className="table-search-members">
-                        {this.GetMembers}
-                        {postItems}
-                    </div>
-                    <br />
-                    <div>
-                        <div className="button-upload-document">
-                            <Button className="upload-document-button">UploadDocument</Button>
+                <div className="documents-body ">
+                        <div className="label-select-document">
+                            <label >
+                                Select Document Type
+                            </label>
                         </div>
-                        <br />
-                    </div>
-                    <div className="button-upload-document">
-                        <Button className="send-email" onClick={this.SendMail}>SendEmail</Button>
-                    </div>
-                    <br />
+                                
+                        <div className="button-upload-document-3">
+                            <button className={this.state.value=="Document"? "btn-active send-email btn-bottom-3": "btn-default send-email btn-bottom-3"} onClick={this.state.value=="Document"? this.SendLOGS: null}>Letter Of Good Standing </button>
+                        </div>
+                        <div className="button-upload-document-2">
+                            <button className={this.state.value2=="Document"? "btn-active send-email btn-bottom-2": "btn-default send-email btn-bottom-2"} onClick={this.state.value2=="Document"? this.SendLOS: null}>Letter Of Status</button>
+                        </div>
+                        <div className="document-requirements"> 
+
+                        </div>
+                        
                 </div>
+                
             </div>
         );
     }
