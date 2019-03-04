d63deaea913b832c95bfbf4e0b151d306266733c
kyle-manganyi <tmanganyi@retrorabbit.co.za>
2019-03-04 08:17:31 +0200

    documents

--- skietbaan_fe/src/components/Documents.js
+++ skietbaan_fe/src/components/Documents.js
@@ -3,6 +3,7 @@ import { Label, Table, Input, Button } from 'reactstrap';
 import '../components/Documents.css';
 import { getCookie } from './cookie.js';
 import {BASE_URL} from '../actions/types.js';
+import { Collapse } from 'react-collapse';
 
 class Documents extends Component {
 
@@ -18,24 +19,24 @@ class Documents extends Component {
       }
 
     componentWillMount(){
-        let token =getCookie("token");
-        fetch("http://localhost:50962/api/Documents/UserLOGS/" + token)
+        let token ="b00847640b84";
+        fetch("http://localhost:56797/api/Documents/UserLOGS/" + token)
         .then(res=>res.json())
         .then(data=>this.setState({value:data}))
 
-        fetch("http://localhost:50962/api/Documents/UserLOS/" + token)
+        fetch("http://localhost:56797/api/Documents/UserLOS/" + token)
         .then(res=>res.json())
         .then(data=>this.setState({value2:data}))
     }
 
     SendLOGS(){
-        let token =getCookie("token");
-        fetch("http://localhost:50962/api/Documents/SendLOGS/" + token)
+        let token ="b00847640b84";
+        fetch("http://localhost:56797/api/Documents/SendLOGS/" + token)
     }
 
     SendLOS(){
-        let token =getCookie("token");
-        fetch("http://localhost:50962/api/Documents/SendLOS/" + token)
+        let token ="b00847640b84";
+        fetch("http://localhost:56797/api/Documents/SendLOS/" + token)
     }
 
    
@@ -64,9 +65,26 @@ class Documents extends Component {
                                 
                         <div className="button-upload-document-3">
                             <button className={this.state.value=="Document"? "btn-active send-email btn-bottom-3": "btn-default send-email btn-bottom-3"} onClick={this.state.value=="Document"? this.SendLOGS: null}>Letter Of Good Standing </button>
+
+                          
+                            <div className="label-select-document">
+                                <label className="testing-align">
+                                    {this.state.value=="Document"? "":"Requirement: \n for good standing wil be paid-up membership with atleast one compittion submited" }
+                                    
+                                </label>
+                            </div>
+                                
                         </div>
                         <div className="button-upload-document-2">
                             <button className={this.state.value2=="Document"? "btn-active send-email btn-bottom-2": "btn-default send-email btn-bottom-2"} onClick={this.state.value2=="Document"? this.SendLOS: null}>Letter Of Status</button>
+                        
+                            <div className="label-select-document">
+                                <label >
+                                    {this.state.value2=="Document"? "":"Requirement: \n 6 ipsc competitions"}
+                                    
+                                </label>
+                            </div>
+
                         </div>
                         <div className="document-requirements"> 
 
