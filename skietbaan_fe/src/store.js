a92f2d915bdd7bde9d8f83c28934040d01dce96d
mthembbu <bmthembu@retrorabbit.co.za>
2019-03-05 08:15:58 +0200

    removed GC code

--- skietbaan_fe/src/store.js
+++ skietbaan_fe/src/store.js
@@ -2,15 +2,12 @@
  * src/store.js
  * With initialState
 */
-import {createStore, applyMiddleware, compose} from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
 const initialState = {};
-const middleware = [thunk];
-const store = createStore(
-    rootReducer,
-    initialState,
-    compose( applyMiddleware(...middleware)));
-  
+const middleware = [ thunk ];
+const store = createStore(rootReducer, initialState, compose(applyMiddleware(...middleware)));
+
 export default store;
