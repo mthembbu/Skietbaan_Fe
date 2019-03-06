b6da12592330e5e5b2414d990374ff5a81bfd9f2
kyle-manganyi <tmanganyi@retrorabbits.co.za>
2019-03-06 16:47:03 +0200

    sorted out icons

--- skietbaan_fe/src/components/Documents.js
+++ skietbaan_fe/src/components/Documents.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
 import '../components/Documents.css';
 import { getCookie } from './cookie.js';
+import { Collapse } from 'react-collapse';
+
 class Documents extends Component {
     constructor(props) {
         super(props);
         this.state = {
           value: "",
           value2: "",
+          collapseFilter: false,
+          collapseFilter2: false
+
         };
         this.SendLOGS = this.SendLOGS.bind(this)
         this.SendLOS = this.SendLOS.bind(this)
       }
 
     componentWillMount(){
-        let token =getCookie("token");
+        let token ="7dfc0e2fd04e";
         fetch("http://api.skietbaan.retrotest.co.za/api/Documents/UserLOGS/" + token)
         .then(res=>res.json())
         .then(data=>this.setState({value:data}))
@@ -26,11 +31,31 @@ class Documents extends Component {
     SendLOGS(){
         let token =getCookie("token");
         fetch("http://api.skietbaan.retrotest.co.za/api/Documents/SendLOGS/" + token)
+
+        if (this.state.collapseFilter) {
+            this.setState({
+                collapseFilter: false
+            });
+        } else {
+            this.setState({
+                collapseFilter: true
+            });
+        }
     }
 
     SendLOS(){
         let token =getCookie("token");
         fetch("http://api.skietbaan.retrotest.co.za/api/Documents/SendLOS/" + token)
+
+        if (this.state.collapseFilter2) {
+            this.setState({
+                collapseFilter2: false
+            });
+        } else {
+            this.setState({
+                collapseFilter2: true
+            });
+        }
     }
 
    
@@ -46,11 +71,29 @@ class Documents extends Component {
                         </div>
                                 
                         <div className="button-upload-document-3">
-                            <button className={this.state.value=="Document"? "btn-active send-email btn-bottom-3":"btn-default send-email btn-bottom-3"} onClick={this.state.value=="Document"? this.SendLOGS: null}> Letter of Good Standing {this.state.value=="Document"? <img src={require("../resources/sendDoc.png")}/>:null}</button>
+                            <button className={this.state.value=="Document"? "btn-active send-email btn-bottom-3":"btn-default send-email btn-bottom-3"} onClick={this.state.value=="Document"? this.SendLOGS: null}> Letter of Good Standing {this.state.value=="Document"? <img className="document-image-icon" src={require("../resources/sendDoc.png")}/>:<img className="document-image-icon" src={require("../resources/gidx.png")}/>}</button>
+
+                            <Collapse isOpened={this.state.collapseFilter}>
+
+                                <div className="documents-collapse">
+                                    Document Sent 
+                                </div>
+                            
+                            </Collapse>
                                 
                         </div>
                             <div className="button-upload-document-2">
-                                <button className={this.state.value2=="Document"? "btn-active send-email btn-bottom-2":"btn-default send-email btn-bottom-2"} onClick={this.state.value2=="Document"? this.SendLOS: null}>Letter of Status {this.state.value2=="Document"? <img src={require("../resources/sendDoc.png")}/>:null}</button>                       
+                                <button className={this.state.value2=="Document"? "btn-active send-email btn-bottom-2":"btn-default send-email btn-bottom-2"} onClick={this.state.value2=="Document"? this.SendLOS: null}>Letter of Status {this.state.value2=="Document"? <img className="document-image-icon" src={require("../resources/sendDoc.png")}/>:<img className="document-image-icon" src={require("../resources/gidx.png")}/>}</button>                       
+
+                                <Collapse isOpened={this.state.collapseFilter2}>
+
+                                <div className="documents-collapse">
+                                    Document Sent
+                                </div>
+                            
+                            </Collapse>
+                            
+                            
                             </div>
                         <div className="documents-rectangle">
                             <div className="label-select-document2"> 
