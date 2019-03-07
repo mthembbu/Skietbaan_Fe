f2c61e9ab4fdcbe27058e173a6816ae72ef35e6d
mthembbu <bmthembu@retrorabbit.co.za>
2019-03-07 14:35:51 +0200

    view competition feature updated to wire-frame designs

--- skietbaan_fe/src/components/ViewComp.js
+++ skietbaan_fe/src/components/ViewComp.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { fetchcomp, 
-		updateByIDcomp 
-	} from '../actions/competition.action';
+		 updateByIDcomp 
+} from '../actions/competition.action';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import '../scss/view-comp.css';
@@ -13,18 +13,18 @@ class ViewComp extends Component {
 			status: [],
 			updatedComp: {}
 		};
-		this.changeActive = this.changeStatus.bind(this);
+		this.changeStatus = this.changeStatus.bind(this);
 		this.onClick = this.onClick.bind(this);
 	}
 
-	onClick(){
-		window.location = "/create";
+	onClick() {
+		window.location = '/create';
 	}
 	// The method that mounts everytime there is an action detected
 	componentWillMount() {
 		this.props.fetchcomp();
 	}
-	
+
 	componentWillReceiveProps(val) {
 		for (let i = 0; i < val.length; i++) {
 			this.state.status.push(val.compOBJ[i].status);
@@ -34,38 +34,37 @@ class ViewComp extends Component {
 	changeStatus(status, i) {
 		const newCompOBJArr = [ ...this.props.compOBJ ]; //cloning an array of competition
 		newCompOBJArr[i].status = !status; //changing the status of a newly cloned array
-		console.log('data status',newCompOBJArr[i].status)
-		this.props.updateByIDcomp(newCompOBJArr[i], i); //updating the competition status by ID in the back-end
+		console.log('data status', newCompOBJArr[i].status);
+		this.props.updateByIDcomp(newCompOBJArr[i], i + 1); //updating the competition status by ID in the back-end
 		this.setState({ compOBJ: newCompOBJArr });
 	}
 	render() {
 		const displayCompetitions = (
 			<div className="page-contents">
-			<table class="table-view-competitions">
-				<tbody>
-					{this.props.compOBJ.map((compVar, i) => (
-						<tr key={compVar.id} className="table-competition-row">
-							<td>
-								<label class="competition-containers">{compVar.name}</label>
-							</td>
-							<td>
-								<div className={compVar.status ? 'activeButton' : 'inactiveButton'}>
-									<button
-										type="button"
-										class="btn btn-outline-secondary "
-										data-toggle="tooltip"
-										title={compVar.status ? ('Deactivate '.concat(compVar.name)) : ('Activate '.concat(compVar.name))}
-										onClick={() => this.changeStatus(compVar.status, i)}
-									>
-										<strong>{String(compVar.status ? 'De-activate' : 'Re-activate')}</strong>
-										<span class="badge badge-light" />
-									</button>
-								</div>
-							</td>
-						</tr>
-					))}
-				</tbody>
-			</table>
+				<table class="table-view-competitions">
+					<tbody>
+						{this.props.compOBJ.map((compVar, i) => (
+							<tr key={compVar.id} className="table-competition-row">
+								<td className="td-col">
+									<label class="competition-containers">{compVar.name}</label>
+								</td>
+								<td className="td-col">
+									<div className={compVar.status ? 'activeButton' : 'inactiveButton'}>
+										<button
+											type="button"
+											class="btn btn-outline-secondary "
+											data-toggle="tooltip"
+											title={compVar.status ? ('Deactivate '.concat(compVar.name)) : ('Activate '.concat(compVar.name))}
+											onClick={() => this.changeStatus(compVar.status, i)}>
+											{String(compVar.status ? 'De-activate' : 'Activate')}
+											<span class="badge badge-light" />
+										</button>
+									</div>
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
 			</div>
 		);
 		return (
