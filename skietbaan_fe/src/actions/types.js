41157f15163cd70d73891fab774677bcefabe758
Bathande <bndaba@retrorabbit.co.za>
2019-03-08 10:01:32 +0200

    f

--- skietbaan_fe/src/actions/types.js
+++ skietbaan_fe/src/actions/types.js
@@ -35,8 +35,7 @@ export const NEW_POST = 'NEW_POST';
 export const NEW_COMP = 'NEW_COMP';
 export const FETCH_COMP = 'FETCH_COMP';
 export const UPDATE_COMP_STATUS = 'UPDATE_COMP_STATUS';
-// export const BASE_URL = "https://api.skietbaan.retrotest.co.za";
-export const BASE_URL = "http://localhost:65182";
+export const BASE_URL = "https://api.skietbaan.retrotest.co.za";
 
 //leaderboard types
 export const FETCH_LEADERBOARDFILTER_DATA = 'FETCH_LEADERBOARDFILTER_DATA';
