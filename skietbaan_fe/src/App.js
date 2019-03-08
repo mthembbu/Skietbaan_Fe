3940b4fffdfa0ee6793ac108e2354324dbb18783
DinolanNaidoo <dnaidoo@retrorabbit.co.za>
2019-03-08 07:59:40 +0200

    Few Fixes

--- skietbaan_fe/src/App.js
+++ skietbaan_fe/src/App.js
@@ -50,32 +50,18 @@ class App extends Component {
 			.catch(function (data) {
 				console.log("error")
 			});
-	}
-
-	componentDidMount(){
-		let token = getCookie("token");
-		fetch(BASE_URL + "/api/features/getuserbytoken/" + token, {
-			method: 'Get',
-			headers: {
-				'Accept': 'application/json',
-				'Content-Type': 'application/json'
+			if(this.state.nav === true){
+				return <NavbarMenu/>
+			}else{
+				return <NavbarMenuUser/>
 			}
-		})
-			.then(response => response.json())
-			.then(data => this.setState({
-				nav:data.admin
-			}))
-			.then(function (data) {})
-			.catch(function (data) {
-				console.log("error")
-			});
 	}
 
 	render() {
 		return (
 			<Provider store={store}>
 				<div className="App">
-				{this.state.nav ? <NavbarMenu />:<NavbarMenuUser />}
+				{this.TypeUser()}
 					<Router history={history}>
 						<Switch>
 							<Route path="/home" component={LeaderboardPage} exact />
