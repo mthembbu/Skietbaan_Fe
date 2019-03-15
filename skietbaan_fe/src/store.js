a69e1eeab3498e5f8b88d3461707001b748988ab
Bathande <bndaba@retrorabbit.co.za>
2019-03-15 09:13:01 +0200

    persisted the store

--- skietbaan_fe/src/store.js
+++ skietbaan_fe/src/store.js
@@ -6,12 +6,18 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import { persistStore, persistReducer } from "redux-persist";
 import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
 
+const persistConfig = {
+  key: "root",
+  storage: storage,
+  blacklist: ["compOBJ"]
+};
 
+const pReducer = persistReducer(persistConfig, rootReducer);
 const initialState = {};
 const middleware = [thunk];
 export const store = createStore(
-  rootReducer,
+  pReducer,
   initialState,
   composeWithDevTools(applyMiddleware(...middleware))
 );
-
+export const persistor = persistStore(store);
