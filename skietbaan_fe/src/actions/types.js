4a20a0f7e22ebea46d1df18742329f21a503b92d
Sibusiso-Retro <smalatji@retrorabbit.co.za>
2019-03-18 17:09:08 +0200

    "fixed leaderboard bugs"

--- skietbaan_fe/src/actions/types.js
+++ skietbaan_fe/src/actions/types.js
@@ -35,17 +35,19 @@ export const NEW_POST = 'NEW_POST';
 export const NEW_COMP = 'NEW_COMP';
 export const FETCH_COMP = 'FETCH_COMP';
 export const UPDATE_COMP_STATUS = 'UPDATE_COMP_STATUS';
-export const BASE_URL = "https://api.skietbaan.retrotest.co.za";
+export const BASE_URL = "https://api.skietbaan.retrotest.co.za/";
 
 //leaderboard types
 export const FETCH_LEADERBOARDFILTER_DATA = 'FETCH_LEADERBOARDFILTER_DATA';
 export const FETCH_LEADERBOARDTABLE_DATA = 'FETCH_LEADERBOARDTABLE_DATA';
+export const UPDATE_SELECTED_COMPETITION = "UPDATE_SELECTED_COMPETITION"
+export const UPDATE_SELECTED_GROUP = "UPDATE_SELECTED_GROUP"
 
 //groups types and constants
-export const URL = "https://api.skietbaan.retrotest.co.za"; 
-export const URLADD = "https://api.skietbaan.retrotest.co.za/api/groups/add"; 
-export const URLUSER = "https://api.skietbaan.retrotest.co.za/api/user"; 
-export const URLGROUP = "https://api.skietbaan.retrotest.co.za/api/groups"; 
+export const URL = BASE_URL; 
+export const URLADD = BASE_URL+"/api/groups/add"; 
+export const URLUSER = BASE_URL+"/api/user"; 
+export const URLGROUP = BASE_URL+"/api/groups"; 
 export const UPDATENAME = 'UPDATENAME';
 export const UPDATE_GROUPNAME = 'UPDATE_GROUPNAME';
 export const CREATEGROUP = 'CREATEGROUP';
