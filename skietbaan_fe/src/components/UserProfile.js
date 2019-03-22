44723e05f1f18aceae7676508b51f0797cfed066
Mandla <mandlamasombuka21@gmail.com>
2019-03-22 15:00:05 +0200

    redirect to register page

--- skietbaan_fe/src/components/UserProfile.js
+++ skietbaan_fe/src/components/UserProfile.js
@@ -5,6 +5,7 @@ import $ from "jquery";
 import { getCookie } from './cookie.js';
 import {BASE_URL} from '../actions/types.js'
 import {Collapse} from 'react-collapse'
+import history from "./history";
 
 class UserProfile extends Component {
     constructor(props){
@@ -23,7 +24,7 @@ class UserProfile extends Component {
     UNSAFE_componentWillMount(){
         var token = getCookie("token");
         if(token === undefined){
-            this.props.history.push('/register-page');
+            history.push('/registerPage');
             return;
         }
         
@@ -39,7 +40,7 @@ class UserProfile extends Component {
             this.setState({awardCompetitions : data})
             if(this.state.awardCompetitions.length == 0){
                 this.Logout();
-                this.props.history.push('/register-page');
+                history.push('/registerPage');
             }
         });
 
@@ -59,7 +60,7 @@ class UserProfile extends Component {
         this.setState(state => ({ collapse: !state.collapse }));
     }
 
-    AnimateAccuracyCircle(counter, element, index){
+    animateAccuracyCircle(counter, element, index){
         if(counter > element.accuracy) return;
         if(counter <= element.accuracy){
             var degreees = (360 * counter) / 100;
@@ -76,7 +77,7 @@ class UserProfile extends Component {
             }
             counter++;
             setTimeout(() => {
-                this.AnimateAccuracyCircle(counter, element, index)
+                this.animateAccuracyCircle(counter, element, index)
             }, 80)
         }
     }
@@ -85,7 +86,7 @@ class UserProfile extends Component {
         This function takes the total score as string and append zeros
         at the front if the total score has less than four digits
     */
-    Tokenize(str){
+    tokenize(str){
         var _array = []
         if(str == null){
             _array = ["0", "0", "0", "0"]
@@ -104,8 +105,8 @@ class UserProfile extends Component {
         return _array
     }
 
-    SetDigits(str, isCompetitionLocked){
-        var _array = this.Tokenize(str)
+    setDigits(str, isCompetitionLocked){
+        var _array = this.tokenize(str)
         return(
             <div className="lay-horizontal">
                 <div className={isCompetitionLocked ? "box box-grey-background" : "box box-white-background"}>
@@ -128,7 +129,7 @@ class UserProfile extends Component {
         )
     }    
     
-    RenderLockedIcon(){
+    renderLockedIcon(){
         return(
             <div className="locked-icon" style={{display: "inline"}}>
                 <img src={require("../resources/awardIcons/locked-icon.png")} alt="lock-icon" />
@@ -136,7 +137,7 @@ class UserProfile extends Component {
         )
     }
 
-    RenderActiveCircle(index){
+    renderActiveCircle(index){
         return(
             <div id={index} className="active-border">
                 <div id={`circle${index}`} className="circle">
@@ -146,7 +147,7 @@ class UserProfile extends Component {
         )
     }
 
-    RenderBulletIcons(){
+    renderBulletIcons(){
         return(
             <div className="bullet-img-scale bullet-padding-right">
                 <img src={require("../resources/awardIcons/bullet.png")} alt="bullet image"></img>
@@ -154,7 +155,7 @@ class UserProfile extends Component {
         )
     }
 
-    RenderRedLockIcon(){
+    renderRedLockIcon(){
         return(
             <div className="lock-img-scale">
                 <img src={require("../resources/awardIcons/red-locked-icon.png")} alt="lock image"></img>
@@ -162,7 +163,7 @@ class UserProfile extends Component {
         )
     }
 
-    CompetitionsStat(element, index){
+    competitionsStat(element, index){
         return(
             <div className="shooting-award push-shooting-award-bottom text-color" key={index}>
                 <Row>
@@ -181,7 +182,7 @@ class UserProfile extends Component {
                                         <Col>
                                             <div className="accuracy-text-align"><label className="accuracy-label">Accuracy</label></div>
                                             <div className="circle-bigger">
-                                               {!element.isCompetitionLocked ? this.RenderActiveCircle(index) : this.RenderRedLockIcon()}
+                                               {!element.isCompetitionLocked ? this.renderActiveCircle(index) : this.renderRedLockIcon()}
                                             </div>
                                             <Row className="center-block-content">
                                                 <Col className="center-icons-col">
@@ -203,12 +204,12 @@ class UserProfile extends Component {
                                             <div className="float-right">
                                                 <div><label className="total-label">Total</label></div>
                                                 <div className="circle-bigger">
-                                                    {!element.isCompetitionLocked ? this.RenderBulletIcons() : null}
+                                                    {!element.isCompetitionLocked ? this.renderBulletIcons() : null}
                                                     <div>
                                                         {!element.isCompetitionLocked ? 
-                                                            <label className="total-text">{element.total}</label> : this.RenderRedLockIcon()}
+                                                            <label className="total-text">{element.total}</label> : this.renderRedLockIcon()}
                                                     </div>
-                                                    {!element.isCompetitionLocked ? this.RenderBulletIcons() : null}
+                                                    {!element.isCompetitionLocked ? this.renderBulletIcons() : null}
                                                 </div>
                                                 <Row className="center-block-content">
                                                 <Col className="center-icons-col">
@@ -232,13 +233,13 @@ class UserProfile extends Component {
                             </Col>
                         </Row>
                     </Col>
-                    <div>{this.AnimateAccuracyCircle(1, element, index)}</div>
+                    <div>{this.animateAccuracyCircle(1, element, index)}</div>
                 </Row>
             </div>
         )
     }
 
-    RenderHoursIcons(){
+    renderHoursIcons(){
         return(
             <div className="lay-horizontal scale-img center-block-content">
                 <img src={this.state.hoursAward.gold ?
@@ -251,10 +252,10 @@ class UserProfile extends Component {
         )
     }
 
-    RenderAllCompetitionsStats(){
+    renderAllCompetitionsStats(){
         let renderArray = []
         this.state.awardCompetitions.forEach((element, index) => {
-            renderArray.push(this.CompetitionsStat(element, index))
+            renderArray.push(this.competitionsStat(element, index))
         })
 
         return renderArray;
