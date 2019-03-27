fe08d75ddf4fd3a6426dea708426b751a26f3f78
kyle-manganyi <tmanganyi@retrorabbits.co.za>
2019-03-27 08:58:06 +0200

    remove imports

--- skietbaan_fe/src/components/ViewGroups.js
+++ skietbaan_fe/src/components/ViewGroups.js
@@ -1,155 +1,167 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import './groups.css';
-import history from './history';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import "./groups.css";
 import {
-	passId,
-	getName,
-	newGroupArrayState,
-	fetchGroups,
-	groupDictionary,
-	pageState,
-	emptyState
-} from '../actions/postActions';
-import { BASE_URL } from '../actions/types';
-import Switch from '@material-ui/core/Switch';
-import group from './GroupImages/Group.png';
-import PropTypes from 'prop-types';
-import { getCookie } from '../components/cookie.js';
+  passId,
+  getName,
+  newGroupArrayState,
+  fetchGroups,
+  groupDictionary,
+  pageState,
+  emptyState
+} from "../actions/postActions";
+import { BASE_URL } from "../actions/types";
+import Switch from "@material-ui/core/Switch";
+import group from "./GroupImages/Group.png";
+import PropTypes from "prop-types";
 
 class ViewGroups extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			posts: [],
-			newArray: [],
-			count: 0,
-			ids: 0,
-			indexs: '',
-			selected: '',
-			deleteState: false
-		};
-		this.onChange = this.onChange.bind(this);
-		this.delete = this.delete.bind(this);
-		this.editGroup = this.editGroup.bind(this);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [],
+      newArray: [],
+      count: 0,
+      ids: 0,
+      indexs: "",
+      selected: "",
+      deleteState: false
+    };
+    this.onChange = this.onChange.bind(this);
+    this.delete = this.delete.bind(this);
+    this.editGroup = this.editGroup.bind(this);
+  }
 
-	async componentWillMount() {
-		await this.props.fetchGroups();
-		await this.props.groupDictionary();
-	}
+  async componentWillMount() {
+    await this.props.fetchGroups();
+    await this.props.groupDictionary();
+  }
 
-	onChange(event) {
-		this.setState({ filterText: event.target.value });
-	}
+  onChange(event) {
+    this.setState({ filterText: event.target.value });
+  }
 
-	editGroup(obj) {
-		this.props.emptyState();
-		this.props.getName(obj.name);
-		this.props.passId(obj.id);
-		this.props.pageState(1);
-	}
+  editGroup(obj) {
+    this.props.emptyState();
+    this.props.getName(obj.name);
+    this.props.passId(obj.id);
+    this.props.pageState(1);
+  }
 
-	async delete(groupId, index) {
-		 if(this.props.groupsList[index].isActive===false){
-		 	this.props.groupsList[index].isActive=true;
-		 }
-		 else{
-		 	this.props.groupsList[index].isActive=false;	
-		 }
-		 this.props.newGroupArrayState([...this.props.groupsList]); 
-		await fetch(BASE_URL + '/api/Groups/' + groupId, {
-			method: 'POST',
-			headers: {
-				Accept: 'application/json',
-				'Content-Type': 'application/json'
-			},
-			body: JSON.stringify(groupId)
-		})
-			.then(function(response) {})
-			.then(function(data) {})
-			.catch(function(data) {});	
-	}
+  async delete(groupId, index) {
+    if (this.props.groupsList[index].isActive === false) {
+      this.props.groupsList[index].isActive = true;
+    } else {
+      this.props.groupsList[index].isActive = false;
+    }
+    this.props.newGroupArrayState([...this.props.groupsList]);
+    await fetch(BASE_URL + "/api/Groups/" + groupId, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(groupId)
+    })
+      .then(function(response) {})
+      .then(function(data) {})
+      .catch(function(data) {});
+  }
 
-	render() {
-		const postitems = (
-			<div className="the-main">
-				<table className="table-member">
-					<tbody>
-						{this.props.groupsList
-							.filter((post) => {
-								return (
-									!this.state.filterText ||
-									post.username.toLowerCase().startsWith(this.state.filterText.toLowerCase()) ||
-									post.email.toLowerCase().startsWith(this.state.filterText.toLowerCase()) ||
-									post.memberID.startsWith(this.state.filterText)
-								);
-							})
-							.map((post, index) => (
-								<tr className="view-group" key={post.id}>
-									<td
-										className={post.isActive === true ? 'first-row' : 'first-row-active'}
-										onClick={() => this.editGroup(post)}
-									>
-										{post.name}
-									</td>
+  render() {
+    const postitems = (
+      <div className="the-main">
+        <table className="table-member">
+          <tbody>
+            {this.props.groupsList
+              .filter(post => {
+                return (
+                  !this.state.filterText ||
+                  post.username
+                    .toLowerCase()
+                    .startsWith(this.state.filterText.toLowerCase()) ||
+                  post.email
+                    .toLowerCase()
+                    .startsWith(this.state.filterText.toLowerCase()) ||
+                  post.memberID.startsWith(this.state.filterText)
+                );
+              })
+              .map((post, index) => (
+                <tr className="view-group" key={post.id}>
+                  <td
+                    className={
+                      post.isActive === true ? "first-row" : "first-row-active"
+                    }
+                    onClick={() => this.editGroup(post)}
+                  >
+                    {post.name}
+                  </td>
 
-									<td className="group-container">
-										{post.isActive === true ? (
-											<div>
-												<img src={group} className="groupIcon" alt="" />
-												<label className="numberOfUser">{this.props.groupDict[post.id]}</label>
-											</div>
-										) : null}
-									</td>
-									<td>
-										<div className="group-view">
-											<Switch
-												color={'primary'}
-												className="Active"
-												focus={true}
-												checked={post.isActive}
-												onClick={() => this.delete(post.id, index)}
-											/>
-										</div>
-									</td>
-								</tr>
-							))}
-					</tbody>
-				</table>
-			</div>
-		);
+                  <td className="group-container">
+                    {post.isActive === true ? (
+                      <div>
+                        <img src={group} className="groupIcon" alt="" />
+                        <label className="numberOfUser">
+                          {this.props.groupDict[post.id]}
+                        </label>
+                      </div>
+                    ) : null}
+                  </td>
+                  <td>
+                    <div className="group-view">
+                      <Switch
+                        color={"primary"}
+                        className="Active"
+                        focus={true}
+                        checked={post.isActive}
+                        onClick={() => this.delete(post.id, index)}
+                      />
+                    </div>
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
+    );
 
-		return (
-			<main className="The-Main">
-				<div className="scrollbar" data-simplebar data-simplebar-auto-hide="false">
-					{postitems}
-				</div>
-			</main>
-		);
-	}
+    return (
+      <main className="The-Main">
+        <div
+          className="scrollbar"
+          data-simplebar
+          data-simplebar-auto-hide="false"
+        >
+          {postitems}
+        </div>
+      </main>
+    );
+  }
 }
 
 ViewGroups.propTypes = {
-	groupDict: PropTypes.shape({
-		Id: PropTypes.arrayOf(PropTypes.number),
-		count: PropTypes.arrayOf(PropTypes.number)
-	})
+  groupDict: PropTypes.shape({
+    Id: PropTypes.arrayOf(PropTypes.number),
+    count: PropTypes.arrayOf(PropTypes.number)
+  })
 };
 
-const mapStateToProps = (state) => ({
-	name: state.posts.groupName,
-	id: state.posts.groupId,
-	groupsList: state.posts.groupsList,
-	groupDict: state.posts.groupDict
+const mapStateToProps = state => ({
+  name: state.posts.groupName,
+  id: state.posts.groupId,
+  groupsList: state.posts.groupsList,
+  groupDict: state.posts.groupDict
 });
 
-export default connect(mapStateToProps, {
-	passId,
-	getName,
-	groupDictionary,
-	newGroupArrayState,
-	pageState,
-	fetchGroups,
-	emptyState
-})(ViewGroups);
+export default connect(
+  mapStateToProps,
+  {
+    passId,
+    getName,
+    groupDictionary,
+    newGroupArrayState,
+    pageState,
+    fetchGroups,
+    emptyState
+  }
+)(ViewGroups);
---
+++ skietbaan_fe/src/components/userDetails.js
@@ -0,0 +1,179 @@
+import React, { Component } from "react";
+import "../components/Documents.css";
+import { getCookie } from "./cookie.js";
+import { Collapse } from "react-collapse";
+import { BASE_URL } from "../actions/types.js";
+
+class userDetails extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sendLogsReturn: "",
+      sendLosReturn: "",
+      collapseFilterLOGS: false,
+      collapseFilterLOS: false
+    };
+    this.sendLOGS = this.sendLOGS.bind(this);
+    this.sendLOS = this.sendLOS.bind(this);
+  }
+
+  componentDidMount() {
+    if (!getCookie("token")) {
+      window.location = "/registerPage";
+    }
+    let token = getCookie("token");
+    fetch(BASE_URL + "/api/Documents/UserLOGS/" + token)
+      .then(res => res.json())
+      .then(data => this.setState({ sendLogsReturn: data }));
+
+    fetch(BASE_URL + "/api/Documents/UserLOS/" + token)
+      .then(res => res.json())
+      .then(data => this.setState({ sendLosReturn: data }));
+  }
+
+  sendLOGS() {
+    let token = getCookie("token");
+    fetch(BASE_URL + "/api/Documents/SendLOGS/" + token, {
+      method: "post",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(token)
+    });
+
+    if (this.state.collapseFilterLOGS) {
+      this.setState({
+        collapseFilterLOGS: false
+      });
+    } else {
+      this.setState({
+        collapseFilterLOGS: true
+      });
+    }
+  }
+
+  sendLOS() {
+    let token = getCookie("token");
+    fetch(BASE_URL + "/api/Documents/SendLOS/" + token, {
+      method: "post",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(token)
+    });
+
+    if (this.state.collapseFilterLOS) {
+      this.setState({
+        collapseFilterLOS: false
+      });
+    } else {
+      this.setState({
+        collapseFilterLOS: true
+      });
+    }
+  }
+
+  render() {
+    if (!getCookie("token")) {
+      window.location = "/registerPage";
+    }
+    return (
+      <div className="documents_background ">
+        <div className="documents-center">
+          <div className="label-select-document">
+            {this.state.sendLogsReturn === "Document" &&
+            this.state.sendLosReturn === "Document"
+              ? "Select Document"
+              : "You've got some shooting to do!"}
+          </div>
+
+          <div className="button-upload-document-3">
+            <button
+              className={
+                this.state.sendLogsReturn === "Document"
+                  ? "Documents-btn-active document-btn-bottom-3"
+                  : "documents-btn-default document-btn-bottom-3"
+              }
+              onClick={
+                this.state.sendLogsReturn === "Document" ? this.sendLOGS : null
+              }
+            >
+              Letter of Good Standing
+              {this.state.sendLogsReturn !== "Document" ? (
+                <img
+                  className="document-image-icon"
+                  src={require("../resources/noDoc.png")}
+                  alt=""
+                />
+              ) : null}
+            </button>
+
+            <div className="document-requirements3">
+              {this.state.sendLogsReturn !== "Document" ? (
+                <div>
+                  <b>Letter of Good Standing:</b>
+                  <p>requires 5 more shoots.</p>
+                </div>
+              ) : null}
+            </div>
+            <Collapse isOpened={this.state.collapseFilterLOGS}>
+              <div className="documents-collapse">
+                Document Sent via email
+                <img
+                  className="document-image-icon"
+                  src={require("../resources/sendDoc.png")}
+                  alt=""
+                />
+              </div>
+            </Collapse>
+          </div>
+          <div className="button-upload-document-2">
+            <button
+              className={
+                this.state.sendLosReturn === "Document"
+                  ? "Documents-btn-active document-btn-bottom-2"
+                  : "documents-btn-default document-btn-bottom-2"
+              }
+              onClick={
+                this.state.sendLosReturn === "Document" ? this.sendLOS : null
+              }
+            >
+              Letter of Status{" "}
+              {this.state.sendLosReturn !== "Document" ? (
+                <img
+                  className="document-image-icon"
+                  src={require("../resources/noDoc.png")}
+                  alt=""
+                />
+              ) : null}
+            </button>
+
+            <div className="document-requirements3">
+              {this.state.sendLosReturn !== "Document" ? (
+                <div>
+                  <b>Letter of Status:</b>
+                  <p>requires further shooting documentation.</p>
+                </div>
+              ) : null}
+            </div>
+
+            <Collapse isOpened={this.state.collapseFilterLOS}>
+              <div className="documents-collapse">
+                Document Sent via email
+                <img
+                  className="document-image-icon"
+                  src={require("../resources/sendDoc.png")}
+                  alt=""
+                />
+              </div>
+            </Collapse>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default userDetails;
