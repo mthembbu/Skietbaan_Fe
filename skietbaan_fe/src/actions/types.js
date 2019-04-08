c75c3a20b375fcb45547cfd885d596a9033655d6
Zintle Skosana <zskosana@retrorabbit.co.za>
2019-04-08 21:39:02 +0200

    .

--- skietbaan_fe/src/actions/types.js
+++ skietbaan_fe/src/actions/types.js
@@ -34,6 +34,7 @@ export const NEW_COMP = "NEW_COMP";
 export const FETCH_COMP = "FETCH_COMP";
 export const FETCH_REQ = "FETCH_REQUIREMENTS";
 export const UPDATE_REQ = "UPDATE_REQUIREMENTS";
+export const CHOOSEPAGE = "CHOOSEPAGE";
 export const UPDATE_COMP_STATUS = "UPDATE_COMP_STATUS";
 export const PARTICIPANTS_PER_COMP = "PARTICIPANTS_PER_COMP";
 export const BASE_URL = "https://api.skietbaan.retrotest.co.za";
