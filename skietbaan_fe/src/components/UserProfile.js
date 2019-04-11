68b9002d94dd1b15b1155311a6572058998e1fc9
Mandla <mandlamasombuka21@gmail.com>
2019-04-11 14:34:38 +0200

    stop the accuracy re-render

--- skietbaan_fe/src/components/UserProfile.js
+++ skietbaan_fe/src/components/UserProfile.js
@@ -28,6 +28,7 @@ class UserProfile extends Component {
         this.mapCompetitionNameToIndex = {}
         this._isMounted = false;
         this.isBestInMonth = false;
+        this.accuracyReRender = true;
     }
 
     UNSAFE_componentWillMount() {
@@ -110,10 +111,11 @@ class UserProfile extends Component {
             counter++;
             const timeout = setTimeout(() => {
                 this.animateAccuracyCircle(counter, element, index)
-            }, 80)
+            }, 50)
             this.timeouts.push(timeout);
         }else{
             $(`#circle${index}`).html(element.accuracy+"%");
+            this.accuracyReRender = false;
         }
     }
 
@@ -121,18 +123,6 @@ class UserProfile extends Component {
         return(
             <div className="locked-icon">
                 <img src={require("../resources/awardIcons/light-red-locked-icon.png")} alt="lock-icon" />
-            </div>
-        )
-    }
-
-    renderActiveCircle(element,index){
-        return(
-            <div id={index} className="active-border">
-                <div id={`circle${index}`} className={element.accuracy.toString().split(".")[0].length > 2 ? 
-                    "circle font-size-15px" : "circle font-size-18px"}>
-                    <label className="accuracy-text">0%</label>
-                </div>
-                {this.animateAccuracyCircle(0, element, index)}
             </div>
         )
     }
@@ -156,6 +146,7 @@ class UserProfile extends Component {
     }
 
     setSelectedCompetition(competitionName){
+        this.accuracyReRender = true;
         this.props.setSelectedCompetition(competitionName);
         this.toggle();
     }
@@ -186,6 +177,18 @@ class UserProfile extends Component {
         return renderArray;
     }
 
+    renderActiveCircle(counter, element,index){
+        return(
+            <div id={index} className="active-border">
+                <div id={`circle${index}`} className={element.accuracy.toString().split(".")[0].length > 2 ? 
+                    "circle font-size-15px" : "circle font-size-18px"}>
+                    <label className="accuracy-text">0%</label>
+                </div>
+                {this.animateAccuracyCircle(counter, element, index)}
+            </div>
+        )
+    }
+
     renderAccuracyCircle(element, index){
         return(
             <div>
@@ -194,7 +197,9 @@ class UserProfile extends Component {
                 </div>
                 <div className="circle-bigger">
                     {!element.isCompetitionLocked ?
-                        (element.accuracy === 0 ? <label className="accuracy-text">0%</label> : this.renderActiveCircle(element,index))
+                        element.accuracy === 0 ? <label className="accuracy-text">0%</label> :
+                        this.accuracyReRender ? this.renderActiveCircle(0,element,index) : 
+                        this.renderActiveCircle(element.accuracy, element,index)
                         : this.renderRedLockIcon(index)}
                 </div>
             </div>
@@ -310,8 +315,12 @@ class UserProfile extends Component {
                     <div className="line adjust-top-line"></div>
                     <Row className="awards-container pad-top-30px">
                         <Col xs={4}sm={4}md={4} className="push-bottom-49px">
-                            {this.renderAccuracyCircle(this.getInitialAward(),
-                                this.getIndexByCompetitionName(this.getInitialAward().competitionName))}
+                            {
+                                this.renderAccuracyCircle(
+                                    this.getInitialAward(),
+                                    this.getIndexByCompetitionName(this.getInitialAward().competitionName)
+                                )
+                            }
                         </Col>
                         <Col xs={7}sm={7}md={7}>
                             <Row className="push-bottom-13px">
