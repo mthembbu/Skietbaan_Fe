8670aa8e5096769e4b4b5042826612357b535bf3
ilzeLourens <ilourens@retrorabbit.co.za>
2019-04-15 16:22:04 +0200

    set timer

--- skietbaan_fe/src/components/userDetails.js
+++ skietbaan_fe/src/components/userDetails.js
@@ -90,14 +90,20 @@ export default class userDetails extends Component {
   render() {
     return (
       <div className="document-center">
-        {this.state.array.length === 0 || this.state.getDataUser === false ?
+        {this.state.getDataUser === false ?
           <div className={this.state.getDataUser ? "hidden" : "loader-container-details"}>
             <div className={this.state.getDataUser ? "hidden" : "loader"}>
             </div>
             <div className={this.state.getDataUser ? "hidden" : "target-loader-image"}>
             </div>
             <div className={this.state.getDataUser ? "hidden" : "loading-message"}>Loading...</div>
-          </div> : (
+
+            {setTimeout(() => {
+              this.state.getDataUser?this.setState({getDataUser: true}):
+              window.location="/login"
+		          }, 8000)}
+          </div> 
+          : (
             <div className="user-details-main-container user-details-container">
 
               <div className="user-details-scrolls">
