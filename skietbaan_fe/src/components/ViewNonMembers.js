9cc98215a03e953d7430e6e02e1e550f8f499f8a
Bathande <bndaba@retrorabbit.co.za>
2019-04-15 15:34:09 +0200

    fixed the nonViewMembers icon and making a person a member

--- skietbaan_fe/src/components/ViewNonMembers.js
+++ skietbaan_fe/src/components/ViewNonMembers.js
@@ -24,7 +24,7 @@ class ViewNonMembers extends Component {
 			height: window.innerHeight,
 			width: window.innerWidth
 		};
-		this.getNonMembers = this.getNonMembers.bind(this);
+		// this.getNonMembers = this.getNonMembers.bind(this);
 		this.getTimeLeft = this.getTimeLeft.bind(this);
 		this.onChangeText = this.onChangeText.bind(this);
 		this.status = this.status.bind(this);
@@ -97,7 +97,12 @@ class ViewNonMembers extends Component {
 			})
 			.then((data) =>
 				this.setState({
-					array: data
+					array: data.map(user=>{
+						return{
+							...user,
+							selected:false
+						}
+					})
 				})
 			);
 	}
@@ -127,11 +132,13 @@ class ViewNonMembers extends Component {
 	}
 
 	updateMember(index) {
+		delete this.state.array[index].selected
 		let RequestObject = {
 			username: this.state.array[index].username,
 			memberID: this.state.membershipsID,
 			memberExpiryDate: this.getCurrentDate() + 'T00:00:00'
 		};
+
 		fetch(BASE_URL + '/api/Features/Update', {
 			method: 'Post',
 			headers: {
@@ -175,8 +182,10 @@ class ViewNonMembers extends Component {
 		this.setState({ membershipsID: event.target.value });
 	}
 
-	onChangeArrow() {
-		this.setState({ arrowChange: !this.state.arrowChange });
+	onChangeArrow=(index)=> {
+		this.setState({membershipsID:""});
+		this.state.array[index].selected=!this.state.array[index].selected;
+		this.forceUpdate();
 	}
 
 	render() {
@@ -202,21 +211,27 @@ class ViewNonMembers extends Component {
 								post.email.toLowerCase().startsWith(this.state.filterText.toLowerCase())
 							);
 						})
-						.map((post, index) => (
-							<tr className="view-members-user" key={post.id}>
+						.map((posts, index) => (
+							<tr className="view-members-user" key={posts.id} >
 								<td className="first-column">
+							
 									<Collapsible
 										trigger={
-											<div className="username-and-email" onClick={this.onChangeArrow}>
+											<div className="username-and-email" onClick={()=>this.onChangeArrow(index)}>
 												<div className="view-non-members-users-email">
-													<b>{post.username}</b>
-													<div className="view-non-members-email">{post.email}</div>
+													<b>{posts.username}</b>
+													<div className="view-non-members-email">{posts.email}</div>
 												</div>
-												<div className="view-non-members-arrow">
+												
+												<div className="view-non-members-arrow" >
+													{posts.selected===true?<img
+														className="view-non-members-image"
+														src={arrowDown }
+													/>:
 													<img
 														className="view-non-members-image"
-														src={this.state.arrowChange ? arrowUp : arrowDown}
-													/>
+														src={arrowUp}
+													/>}
 												</div>
 											</div>
 										}
@@ -235,11 +250,11 @@ class ViewNonMembers extends Component {
 											</div>
 											<div>
 												<input
+													name
 													type="date"
 													className="view-non-members-text-boxes"
 													id="expdate"
 													value={this.getCurrentDate()}
-													onChange={this.handleChange}
 												/>
 											</div>
 											<div>
