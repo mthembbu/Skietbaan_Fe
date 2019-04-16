f6453f6dbbb50cb2f2f83da9a8f60e2129c49b16
ilzeLourens <ilourens@retrorabbit.co.za>
2019-04-16 16:44:40 +0200

    allow for full stop

--- skietbaan_fe/src/components/Validators.js
+++ skietbaan_fe/src/components/Validators.js
@@ -4,7 +4,7 @@ export function validatePassword(str) {
 }
 
 export function validateEmail(email) {
-  const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const re = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
   return re.test(String(email));
 }
 
