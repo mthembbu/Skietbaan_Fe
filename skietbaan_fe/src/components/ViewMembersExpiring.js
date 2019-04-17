39e10f97893b1a79283ff6ca4be2fd4f3520acc9
Bathande <bndaba@retrorabbit.co.za>
2019-04-17 13:28:34 +0200

    made changes to the expiring members

--- skietbaan_fe/src/components/ViewMembersExpiring.js
+++ skietbaan_fe/src/components/ViewMembersExpiring.js
@@ -23,7 +23,7 @@ class ViewMembersExpiring extends Component {
 			height: window.innerHeight,
 			width: window.innerWidth,
 			getData: false,
-			exportMsg:false
+			exportMsg: false
 		};
 		this.getExpiringMembers = this.getExpiringMembers.bind(this);
 		this.getTimeLeft = this.getTimeLeft.bind(this);
@@ -134,7 +134,7 @@ class ViewMembersExpiring extends Component {
 	updateMember(index) {
 		let RequestObject = {
 			username: this.state.array[index].username,
-			memberExpiryDate: this.getCurrentDate() + 'T00:00:00'
+			memberExpiryDate:this.getCurrentDate() + 'T00:00:00'
 		};
 		fetch(BASE_URL + '/api/Features/RenewMembership', {
 			method: 'Post',
@@ -182,6 +182,7 @@ class ViewMembersExpiring extends Component {
 		let date = curr.toISOString().substr(0, 10);
 		return date;
 	}
+
 	ExportData = () => {
 		this.setState({ exportMsg: true })
 	}
@@ -199,79 +200,88 @@ class ViewMembersExpiring extends Component {
 			});
 		}
 		const postItems = (
-			 <div>
-			{(this.state.array.length === 0 && this.state.getData===true)? <div className="view-non-error-container"><label className="view-non-error-msg">No members have been created yet.</label></div>:
-			<table striped hover condensed className="table-member">
-				<tbody>
-					{this.state.array
-						.filter((post) => {
-							return (
-								!this.state.filterText ||
-								post.username.toLowerCase().startsWith(this.state.filterText.toLowerCase()) ||
-								post.email.toLowerCase().startsWith(this.state.filterText.toLowerCase()) ||
-								post.memberID.startsWith(this.state.filterText)
-							);
-						})
-						.map((post, index) => (
-							<tr className="view-members-user" key={post.id}>
-								<td className="first-column">
-									<Collapsible
-										trigger={
-											<div className="username-and-email">
-												<div className="view-members-username-email">
-													<b>{post.username}</b>
-													<div className="view-non-members-email">{post.email}</div>
-												</div>
-												<div className="view-exp-members-icon">
-													<img
-														src={memberIcon}
-														className="membership-icon"
-														alt="Is a Member"
-													/>
-												</div>
-												<div className="expiry-time-column">
-													<div
-														className={
-															this.status(this.state.timeLeftOnMembership[index]) ? (
-																'bad'
-															) : (
-																	'okay'
-																)
-														}
-													>
-														<div>
-															<b>
-																{post.memberExpiryDate
-																	.substring(0, 10)
-																	.split('-')
-																	.join('/')}
-															</b>
-															<div>{this.state.timeLeftOnMembership[index]} Months</div>
+			<div>
+				{(this.state.array.length === 0 && this.state.getData === true) ? <div className="view-non-error-container"><label className="view-non-error-msg">No users expiring yet.</label></div> :
+					<table striped hover condensed className="table-member">
+						<tbody>
+							{this.state.array
+								.filter((post) => {
+									return (
+										!this.state.filterText ||
+										post.username.toLowerCase().startsWith(this.state.filterText.toLowerCase()) ||
+										post.email.toLowerCase().startsWith(this.state.filterText.toLowerCase()) ||
+										post.memberID.startsWith(this.state.filterText)
+									);
+								})
+								.map((post, index) => (
+									<tr className="view-members-user" key={post.id}>
+										<td className="first-column">
+											<Collapsible
+												trigger={
+													<div className="username-and-email">
+														<div className="view-members-username-email">
+															<b>{post.username}</b>
+															<div className="view-non-members-email">{post.email}</div>
+														</div>
+														<div className="view-exp-members-icon">
+															<img
+																src={memberIcon}
+																className="membership-icon"
+																alt="Is a Member"
+															/>
+														</div>
+														<div className="expiry-time-column">
+															<div
+																className={
+																	this.status(this.state.timeLeftOnMembership[index]) ? (
+																		'bad'
+																	) : (
+																			'okay'
+																		)
+																}
+															>
+																<div>
+																	<b>
+																		{post.memberExpiryDate
+																			.substring(0, 10)
+																			.split('-')
+																			.join('/')}
+																	</b>
+																	<div>{this.state.timeLeftOnMembership[index]} Months</div>
+																</div>
+															</div>
 														</div>
 													</div>
-												</div>
-											</div>
-										}
-									>
-										<div className="renew-container">
-											<button
-												className="view-exp-members"
-												onClick={() => this.updateMember(index)}
+												}
 											>
-												RENEW
+												<div>
+													<input
+														type="date"
+														className="view-non-members-text-boxes"
+														id="expdate"
+														value={this.state.datevalue}
+														onChange={this.handleDateChange}
+													/>
+												</div>
+												<div className="renew-container">
+													<button
+														className="view-exp-members"
+														onClick={() => this.updateMember(index)}
+													>
+														RENEW
 											</button>
-										</div>
-									</Collapsible>
-								</td>
-							</tr>
-						))}
-				</tbody>
-			</table>}</div>
+												</div>
+											</Collapsible>
+										</td>
+									</tr>
+								))}
+						</tbody>
+					</table>}</div>
 		);
 		return (
 			<div className="centre-view-member">
 				<div className="username-search">
-				<Row>
+					<Row>
 						<Col>
 							<div className="search">
 								<input
@@ -305,9 +315,9 @@ class ViewMembersExpiring extends Component {
 					<div className={this.state.getData === true ? "hidden" : "loading-message-members"}>Loading...</div>
 				</div>
 				{this.state.exportMsg === false ?
-				<div className="table-search-members" style={{ height: this.getBodyHeight() }}>
-					{postItems}
-				</div>:<div className="exportMsg-container"><label className="exportMsg-responce">
+					<div className="table-search-members" style={{ height: this.getBodyHeight() }}>
+						{postItems}
+					</div> : <div className="exportMsg-container"><label className="exportMsg-responce">
 						SBmembers.csv sent to fs@retrorabbit.co.za
 				</label>
 						<img
